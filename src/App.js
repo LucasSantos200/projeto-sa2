3f84b96f166bc9e33a9f31cd29f2adebf0d2fc09
Lucas Santos <lucas_santos200@estudante.sesisenai.org.br>
2026-07-20 17:02:34 -0300

    sinceramente nao lembro

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        
+        <div className='header'>
+          Seja Bem Vindo à 
+          <br/>
+          LevaComigo!
+        </div>
+        
+        
+        <div className='pagina'>
+          Insira as Informações
+        </div>
+        <div>
+          <input className='esqueci2' type='text' placeholder='Digite seu Nome'/>
+          <input className='esqueci2' type='text' placeholder='Digite seu Telefone'/>
+          <input className='esqueci2' type='text' placeholder='Digite seu CEP'/>
+          <br/>
+          <input className='esqueci' type='submit'/>
+        </div>
+      
+      <div className='box'>
+        <p>Nome:</p>
+        <p>Telefone: <span>{}</span></p>
+        <p>CEP: <span>{}</span></p>
+        <p>Rua: <span>{}</span></p>
+        <p>Bairro: <span>{}</span></p>
+        <p>Cidade: <span>{}</span></p>
+      </div>
+      
+      
+      </div>
   );
 }
 
